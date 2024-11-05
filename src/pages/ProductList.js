f09bb6f78197712a7e21e5775de04f8fadd220a6
Zara8170 <wlsgusan123@gmail.com>
2024-11-05 16:15:37 +0900

    ProductList 수정

--- src/pages/ProductList.js
+++ src/pages/ProductList.js
@@ -10,128 +10,46 @@ import "./style.css";
 import myImage from "./../images/bts/키링/BTS방탄 굿즈 스트랩 키링.jpg";
 // import required modules
 import { Navigation } from "swiper/modules";
+import SwiperItem from "../components/SwiperItem";
 
 export default function App() {
   return (
     <>
       <section>
         <h3>악세서리</h3>
-        <Swiper
-          navigation={true}
-          modules={[Navigation]}
-          className="mySwiper"
-          slidesPerView={4}
-          spaceBetween={50}
-        >
+        <Swiper navigation={true} modules={[Navigation]} className="mySwiper" slidesPerView={4} spaceBetween={50}>
           <SwiperSlide>
-            <div>
-              <img src={myImage} alt="BTS 키링" />
-              <strong>메뉴이름</strong>
-              <span>
-                <s>원래가격</s>
-                <em>할인가격</em>
-              </span>
-            </div>
+            <SwiperItem />
           </SwiperSlide>
           <SwiperSlide>
-            <div>
-              <img src={myImage} alt="BTS 키링" />
-              <strong>메뉴이름</strong>
-              <span>
-                <s>원래가격</s>
-                <em>할인가격</em>
-              </span>
-            </div>
+            <SwiperItem />
           </SwiperSlide>
           <SwiperSlide>
-            <div>
-              <img src={myImage} alt="BTS 키링" />
-              <strong>메뉴이름</strong>
-              <span>
-                <s>원래가격</s>
-                <em>할인가격</em>
-              </span>
-            </div>
+            <SwiperItem />
           </SwiperSlide>
           <SwiperSlide>
-            <div>
-              <img src={myImage} alt="BTS 키링" />
-              <strong>메뉴이름</strong>
-              <span>
-                <s>원래가격</s>
-                <em>할인가격</em>
-              </span>
-            </div>
+            <SwiperItem />
           </SwiperSlide>
           <SwiperSlide>
-            <div>
-              <img src={myImage} alt="BTS 키링" />
-              <strong>메뉴이름</strong>
-              <span>
-                <s>원래가격</s>
-                <em>할인가격</em>
-              </span>
-            </div>
+            <SwiperItem />
           </SwiperSlide>
           <SwiperSlide>
-            <div>
-              <img src={myImage} alt="BTS 키링" />
-              <strong>메뉴이름</strong>
-              <span>
-                <s>원래가격</s>
-                <em>할인가격</em>
-              </span>
-            </div>
+            <SwiperItem />
           </SwiperSlide>
           <SwiperSlide>
-            <div>
-              <img src={myImage} alt="BTS 키링" />
-              <strong>메뉴이름</strong>
-              <span>
-                <s>원래가격</s>
-                <em>할인가격</em>
-              </span>
-            </div>
+            <SwiperItem />
           </SwiperSlide>
           <SwiperSlide>
-            <div>
-              <img src={myImage} alt="BTS 키링" />
-              <strong>메뉴이름</strong>
-              <span>
-                <s>원래가격</s>
-                <em>할인가격</em>
-              </span>
-            </div>
+            <SwiperItem />
           </SwiperSlide>
           <SwiperSlide>
-            <div>
-              <img src={myImage} alt="BTS 키링" />
-              <strong>메뉴이름</strong>
-              <span>
-                <s>원래가격</s>
-                <em>할인가격</em>
-              </span>
-            </div>
+            <SwiperItem />
           </SwiperSlide>
           <SwiperSlide>
-            <div>
-              <img src={myImage} alt="BTS 키링" />
-              <strong>메뉴이름</strong>
-              <span>
-                <s>원래가격</s>
-                <em>할인가격</em>
-              </span>
-            </div>
+            <SwiperItem />
           </SwiperSlide>
           <SwiperSlide>
-            <div>
-              <img src={myImage} alt="BTS 키링" />
-              <strong>메뉴이름</strong>
-              <span>
-                <s>원래가격</s>
-                <em>할인가격</em>
-              </span>
-            </div>
+            <SwiperItem />
           </SwiperSlide>
         </Swiper>
       </section>
