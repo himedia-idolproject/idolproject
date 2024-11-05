5ad9069cf28441c96577b639a8f8bde43b4a3faf
Zara8170 <wlsgusan123@gmail.com>
2024-11-05 12:50:16 +0900

    추가 수정

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import Layout from "./components/Layout";
 import Home from "./pages/Home";
 import Order from "./pages/Order";
 import Payment from "./pages/Payment";
-import Product from "./pages/Product";
+import ProductList from "./pages/ProductList";
+import ProductDetail from "./pages/ProductDetail";
 import store from "./reduxComponents/store";
 
 function App() {
@@ -15,7 +16,8 @@ function App() {
         <Layout>
           <Routes>
             <Route path="/" element={<Home />}></Route>
-            <Route path="/product" element={<Product />}></Route>
+            <Route path="/product/:idolGroup" element={<ProductList />}></Route>
+            <Route path="/product/:idolGroup/:id" element={<ProductDetail />}></Route>
             <Route path="/order" element={<Order />}></Route>
             <Route path="/payment" element={<Payment />}></Route>
           </Routes>
