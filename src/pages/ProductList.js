671a4778638c7e9b4701fbe51a4c5033fa258e1e
happyvirus77 <happyviruscom7777777@gmail.com>
2024-11-05 16:42:04 +0900

    product 레이아웃 제작

--- src/pages/ProductList.js
+++ src/pages/ProductList.js
@@ -8,6 +8,8 @@ import "swiper/css/navigation";
 import "./style.css";
 
 import myImage from "./../images/bts/키링/BTS방탄 굿즈 스트랩 키링.jpg";
+import myImage2 from "./../images/bts/키링/BTS 피규어 키링.jpg";
+
 // import required modules
 import { Navigation } from "swiper/modules";
 
@@ -35,7 +37,7 @@ export default function App() {
           </SwiperSlide>
           <SwiperSlide>
             <div>
-              <img src={myImage} alt="BTS 키링" />
+              <img src={myImage2} alt="BTS 키링" />
               <strong>메뉴이름</strong>
               <span>
                 <s>원래가격</s>
